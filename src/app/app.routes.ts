8306ee42bb70c23d3ad68ff633f14ad5d57ab8cf
Oskar <oskar.plichta@spantium.com>
2024-11-06 21:13:08 +0100

    Created pizza-builder, pizza-preview and order-form.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { PizzaBuilderComponent } from './components/pizza-builder/pizza-builder.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: PizzaBuilderComponent },
+  ];
