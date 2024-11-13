447d464763d517aff55f2884298e8459a362dacd
Oskar <oskar.plichta@spantium.com>
2024-11-13 16:59:48 +0100

    Updated setup Jest testing

--- src/app/components/pizza-creator/pizza-creator.component.spec.ts
+++ src/app/components/pizza-creator/pizza-creator.component.spec.ts
@@ -1,23 +1,34 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
 import { PizzaCreatorComponent } from './pizza-creator.component';
 
 describe('PizzaCreatorComponent', () => {
   let component: PizzaCreatorComponent;
   let fixture: ComponentFixture<PizzaCreatorComponent>;
+  let store: MockStore;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PizzaCreatorComponent]
-    })
-    .compileComponents();
+      imports: [PizzaCreatorComponent],
+      providers: [
+        provideMockStore({ initialState: {} })
+      ]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(PizzaCreatorComponent);
     component = fixture.componentInstance;
+    store = TestBed.inject(MockStore);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(PizzaCreatorComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h2')?.textContent).toContain('Choose your pizzas');
   });
 });
--- src/app/components/pizza-size/pizza-size.component.spec.ts
+++ src/app/components/pizza-size/pizza-size.component.spec.ts
@@ -1,19 +1,24 @@
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { PizzaSizeComponent } from './pizza-size.component';
 
 describe('PizzaSizeComponent', () => {
   let component: PizzaSizeComponent;
   let fixture: ComponentFixture<PizzaSizeComponent>;
+  let store: MockStore;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PizzaSizeComponent]
+      imports: [PizzaSizeComponent],
+      providers: [
+        provideMockStore({ initialState: {} })
+      ]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(PizzaSizeComponent);
     component = fixture.componentInstance;
+    store = TestBed.inject(MockStore);
     fixture.detectChanges();
   });
 
