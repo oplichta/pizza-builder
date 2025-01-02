cd179ea81c5b926611405cb7df08e4fb0f96f419
Oskar <oskar.plichta@spantium.com>
2025-01-02 16:12:06 +0100

    Fixed mobile view.

--- src/app/components/pizza-size/pizza-size.component.ts
+++ src/app/components/pizza-size/pizza-size.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, forwardRef } from '@angular/core';
+import { Component, forwardRef, HostListener, OnInit } from '@angular/core';
 import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
 import { PizzaSize } from '../../store/order.models';
 
@@ -16,16 +16,30 @@ import { PizzaSize } from '../../store/order.models';
         },
     ],
 })
-export class PizzaSizeComponent implements ControlValueAccessor {
+export class PizzaSizeComponent implements ControlValueAccessor, OnInit {
     value = PizzaSize.Small;
     sizes = [
         { type: PizzaSize.Small, centimeters: 30 },
         { type: PizzaSize.Medium, centimeters: 40 },
         { type: PizzaSize.Large, centimeters: 50 },
     ];
+    isMobileView: boolean = false;
 
     private onChange: (value: PizzaSize) => void = () => {};
     private onTouched: () => void = () => {};
+
+    ngOnInit(): void {
+      this.checkScreenSize();
+    }
+  
+    @HostListener('window:resize', ['$event'])
+    onResize(event: any): void {
+      this.checkScreenSize();
+    }
+  
+    private checkScreenSize(): void {
+      this.isMobileView = window.innerWidth < 391;
+    }
 
     onChangeSize(value: PizzaSize) {
         this.value = value;
